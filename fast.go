e2ac5f179aed2cb0f29293272afb5afc0d7a4457
joker <muratbekauyez>
2021-05-25 15:19:26 +0600

    edit

--- fast.go
+++ fast.go
@@ -1,10 +1,51 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"hw_bench3/user"
 	"io"
+	"os"
+	"strings"
 )
 
 // вам надо написать более быструю оптимальную этой функции
 func FastSearch(out io.Writer) {
-	SlowSearch(out)
+	file, err := os.Open(filePath)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	seenBrowsers := make(map[string]bool)
+	user := user.User{}
+
+	sc := bufio.NewScanner(file)
+
+	for i := 0; sc.Scan(); i++ {
+		err := user.UnmarshalJSON([]byte(sc.Text()))
+		if err != nil {
+			panic(err)
+		}
+
+		isAndroid := false
+		isMSIE := false
+
+		for _, browser := range user.Browsers {
+			if strings.Contains(browser, "Android") {
+				isAndroid = true
+			}
+			if strings.Contains(browser, "MSIE") {
+				isMSIE = true
+			}
+		}
+
+		if !(isAndroid && isMSIE) {
+			continue
+		}
+
+		email := strings.Replace(user.Email, "@", " [at] ", -1)
+		fmt.Fprintf(out, "[%d] %s <%s>\n", i, user.Name, email)
+	}
+	fmt.Fprintln(out, "\nTotal unique browsers", len(seenBrowsers))
 }
---
+++ go.mod
@@ -0,0 +1,5 @@
+module "hw_bench3"
+
+go 1.16
+
+require github.com/mailru/easyjson v0.7.7
